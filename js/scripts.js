ec3203dacf76f39f4ef17e9858e4ec0159568300
Cameron Poore and Katarina Tuttle <cameron+katarina@>
2016-06-13 11:38:44 -0700

    Added README file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 //business logic
-function Contact(first, last) {
+function Contact(first, last, street, city, state, country) {
   this.firstName = first;
   this.lastName = last;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.country = country;
+
 }
 
 // user interface logic
@@ -11,13 +16,25 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedstreet = $("input#street").val();
+    var inputtedcity = $("input#city").val();
+    var inputtedstate = $("input#state").val();
+    var inputtedcountry = $("input#country").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedstreet, inputtedcity, inputtedstate, inputtedcountry);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.street +   " "+ newContact.city + " " + newContact.state +"</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.country + "</span></li>");
+
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#street").val("");
+    $("input#city").val("");
+    $("input#state").val("");
+    $("input#country").val("");
   });
 });
 $(".contact").last().click(function() {
@@ -25,4 +42,8 @@ $(".contact").last().click(function() {
   $("#show-contact h2").text(newContact.firstName);
   $(".first-name").text(newContact.firstName);
   $(".last-name").text(newContact.lastName);
+  $(".street").text(newContact.street);
+  $(".city").text(newContact.city);
+  $(".state").text(newContact.state);
+  $(".country").text(newContact.country);
 });
